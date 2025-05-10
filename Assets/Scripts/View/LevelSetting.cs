2005c02020b3c4a597ce8900217445930f522911
epuir <130913500+epuir@users.noreply.github.com>
2025-05-11 03:36:36 +0800

    ui

---
+++ Assets/Scripts/View/LevelSetting.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace View
+{
+    public class LevelSetting:MonoBehaviour
+    {
+        public GameObject UI;
+        public void OpenUI()
+        {
+            UI.SetActive(true);
+        }
+
+        public void CloseUI()
+        {
+            UI.SetActive(!true);
+        }
+
+        public void ReturnSelect()
+        {
+            SceneManager.LoadScene("Choose");
+        }
+        
+    }
+}
