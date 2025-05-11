fb27e1585e0a5d31f6654f3d2cbd264baad224a1
epuir <130913500+epuir@users.noreply.github.com>
2025-05-12 00:20:51 +0800

    音频

--- Assets/Scripts/AudioManage.cs
+++ Assets/Scripts/AudioManage.cs
@@ -1,18 +1,41 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class AudioManage : MonoBehaviour
 {
-    public AudioClip MainBgm;
-    private AudioSource audio;
+    private List<AudioSource> Audios = new List<AudioSource>();
+    private List<AudioSource> UsedAudio = new List<AudioSource>();
+
     private void Start()
     {
-        audio = gameObject.AddComponent<AudioSource>();
-        audio.clip = MainBgm;
-        audio.Play();
         DontDestroyOnLoad(gameObject);
-    } 
+    }
+
+    public void Play(AudioClip tclip)
+    {
+        AudioSource a;
+        if (Audios.Count > 0)
+        {
+            a = Audios[0];
+            UsedAudio.Add(a);
+            Audios.Remove(a);
+        }
+        else
+        {
+            a  = gameObject.AddComponent<AudioSource>();
+            UsedAudio.Add(a);
+        }
+        asyncplay(a);
+    }
+
+    async void asyncplay(AudioSource a)
+    {
+        await UniTask.WaitUntil(() => a.isPlaying);
+        UsedAudio.Remove(a);
+        Audios.Add(a);
+    }
+    
     
 }
